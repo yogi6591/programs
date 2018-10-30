7b9b2ca8a63c6543ec3e572efa84981b1695a4d6
Rahul Kumar Tiwari <31995292+Rahul-Kumar-Tiwari@users.noreply.github.com>
2018-10-30 16:01:07 +0530

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -1,7 +1,8 @@
 import numpy
-N,M=map(int, input().split())
+N,M=map(int, input().split()) 
 numpy.set_printoptions(sign=' ')
 print(numpy.eye(N,M,k=1))
+#simple program to prit the matrix with the help of numpy
 
 
 
